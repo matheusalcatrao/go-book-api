105a92bfaa7efd5a6d4f211cadce78e54a144acb
Matheus Alcatrão <matheusalcatrao@hotmail.com>
2024-09-04 16:58:02 -0300

    database url

--- db/db.go
+++ db/db.go
@@ -26,7 +26,7 @@ type User struct {
 
 
 func InitDB() {
-	dsn := "host=localhost user=admin password=password dbname=db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := os.Getenv("DATABASE_URL")
 	var err error
 	Database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
